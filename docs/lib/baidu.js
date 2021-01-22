603504305d694d4662b8de88423d1c539137b537
唐亮 <tang97155@163.com>
2021-01-22 16:02:35 +0800

    fix(index): 新增百度统计插件

---
+++ docs/lib/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?e5d43ec24bfb532a1380f3c014d40748";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
