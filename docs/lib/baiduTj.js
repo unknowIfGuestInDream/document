c01da753f4b176c96945f1b36c3f27e80a9a4f02
唐亮 <tang97155@163.com>
2021-01-22 16:09:36 +0800

    fix(index): 修改百度统计插件

--- docs/lib/baiduTj.js
+++ docs/lib/baiduTj.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
     var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?e5d43ec24bfb532a1380f3c014d40748";
+    hm.src = "https://hm.baidu.com/hm.js?abdbaff41ac41f5bbbad7936ffb6c31b";
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(hm, s);
 })();
