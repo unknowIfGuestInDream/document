9614aa38f66cc802a0f57396e1cf00379fb7e05c
梦里不知身是客 <tang97155@163.com>
2025-04-23 15:04:14 +0800

    ci: Update baiduTj.js
    Signed-off-by: 梦里不知身是客 <tang97155@163.com>

--- docs/lib/baiduTj.js
+++ docs/lib/baiduTj.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
     var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?abdbaff41ac41f5bbbad7936ffb6c31b";
+    hm.src = "https://hm.baidu.com/hm.js?3b7c686f71609480d78a182b4fdf33d4";
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(hm, s);
 })();
